0bfe434cdb04fb330ed18db846742fdcf1080b4b
rustekapl <rustekapl@gmail.com>
2022-11-02 08:00:49 +0200

    complete day5 tasks

---
+++ src/main/java/day5/Motorbike.java
@@ -0,0 +1,26 @@
+package day5;
+
+class Motorbike {
+    String model;
+    String color;
+    int year;
+
+    Motorbike(String model, String color, int year) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+}
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -12,27 +12,3 @@ public class Task2 {
 
     }
 }
-class Motorbike{
-    String model;
-    String color;
-    int year;
-
-    Motorbike(String model, String color, int year){
-        this.model=model;
-        this.color=color;
-        this.year=year;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public int getYear() {
-        return year;
-    }
-    
-}
