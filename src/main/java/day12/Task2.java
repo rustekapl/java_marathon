06cff88c3f202edef26b2ca3e2e76495d510f967
rustekapl <rustekapl@gmail.com>
2022-11-09 10:28:21 +0200

    day12 changed ArrayList<String> numbers on List<Integer> numbers

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -1,10 +1,11 @@
 package day12;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Task2 {
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<>();
 
         fill(0, 30, numbers);
         fill(300, 350, numbers);
@@ -14,7 +15,7 @@ public class Task2 {
 
     }
 
-    public static void fill(int min, int max, ArrayList<Integer> numbers) {
+    public static void fill(int min, int max, List<Integer> numbers) {
         for (int i = min; i < max; i++) {
             if (i % 2 == 0) {
                 numbers.add(i);
