a6328713d6345663ef1b9275a26c57cc48677c30
rustekapl <rustekapl@gmail.com>
2022-11-09 14:09:44 +0200

    complete day13 tasks

---
+++ src/main/java/day13/MessageDatabase.java
@@ -0,0 +1,28 @@
+package day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageDatabase {
+    public static List<Message> messages = new ArrayList<>();
+
+    public static void addNewMessage(User u1, User u2, String text) {
+        messages.add(new Message(u1, u2, text));
+    }
+
+    public static List<Message> getMessages() {
+        return messages;
+    }
+
+    public static void showDialog(User u1, User u2) {
+        for (Message message : messages) {
+            if (u1.getUsername().equals(message.getSender().getUsername())
+                    && u2.getUsername().equals(message.getReceiver().getUsername()) || u2.getUsername().equals(message.getSender().getUsername())
+                    && u1.getUsername().equals(message.getReceiver().getUsername())) {
+                System.out.println(message.getSender().getUsername() + ": " + message.getText());
+            }
+        }
+    }
+
+
+}
--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -2,6 +2,28 @@ package day13;
 
 public class Task1 {
     public static void main(String[] args) {
+        User user1 = new User("user1");
+        User user2 = new User("user2");
+        User user3 = new User("user3");
+
+        user1.sendMessage(user2,"Hi!");
+        user1.sendMessage(user2,"How are you?");
+
+        user2.sendMessage(user1,"Hi!");
+        user2.sendMessage(user1,"I'm fine!");
+        user2.sendMessage(user1,"And you?");
+
+        user3.sendMessage(user1,"Hello!");
+        user3.sendMessage(user1,"What are you doing?");
+        user3.sendMessage(user1,"Let's go to the park");
+
+        user1.sendMessage(user3,"Hi!");
+        user1.sendMessage(user3,"I'm busy today");
+        user1.sendMessage(user3,"Maybe nex weekend?");
+
+        user3.sendMessage(user1,"Ok. See you!");
+
+        MessageDatabase.showDialog(user1,user3);
 
     }
 }
