f9dd5263490cad871b0ce2bc7fe5f28feefc03d9
rustekapl <rustekapl@gmail.com>
2022-11-16 15:27:14 +0200

    day17 task2 добавлен метод очистки поля day17 task2 добавлен метод заполнения нужных полей фигурами

--- src/main/java/day17/ChessBoard.java
+++ src/main/java/day17/ChessBoard.java
@@ -1,5 +1,7 @@
 package day17;
 
+import java.util.Scanner;
+
 public class ChessBoard {
     private ChessPiece[][] chessPieces;
 
@@ -14,5 +16,110 @@ public class ChessBoard {
             }
             System.out.println();
         }
+    }
+
+    public void clearChessBoard() {
+        for (int i = 0; i < chessPieces.length; i++) {
+            for (int j = 0; j < chessPieces[i].length; j++) {
+                chessPieces[i][j] = ChessPiece.EMPTY;
+            }
+        }
+    }
+
+    public void initChessBoard() {
+        boolean exit = false;
+
+        while (!exit) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите горизонтальные координаты ячейки (a,b,c,d,e,f,g,h)");
+            String xCoordinate = scanner.nextLine();
+            int x = 0;
+            switch (xCoordinate) {
+                case "h":
+                    x = 7;
+                    break;
+                case "g":
+                    x = 6;
+                    break;
+                case "f":
+                    x = 5;
+                    break;
+                case "e":
+                    x = 4;
+                    break;
+                case "d":
+                    x = 3;
+                    break;
+                case "c":
+                    x = 2;
+                    break;
+                case "b":
+                    x = 1;
+                    break;
+                case "a":
+                    x = 0;
+                    break;
+
+
+            }
+            System.out.println("введите вертикальные координаты (1,2,3,4,5,6,7,8)");
+            int yCoordinate = Integer.parseInt(scanner.nextLine());
+            int y = 8 - yCoordinate;
+
+            System.out.println("введите название фигуры (king, queen, rook, bishop, knight, pawn)");
+            String name = scanner.nextLine();
+            System.out.println("введите цвет фигуры (w, b)");
+            String color = scanner.nextLine();
+            if ("white".equalsIgnoreCase(color)) {
+                switch (name) {
+                    case "king":
+                        chessPieces[x][y] = ChessPiece.KING_WHITE;
+                        break;
+                    case "queen":
+                        chessPieces[x][y] = ChessPiece.QUEEN_WHITE;
+                        break;
+                    case "rook":
+                        chessPieces[x][y] = ChessPiece.ROOK_WHITE;
+                        break;
+                    case "bishop":
+                        chessPieces[x][y] = ChessPiece.BISHOP_WHITE;
+                        break;
+                    case "knight":
+                        chessPieces[x][y] = ChessPiece.KNIGHT_WHITE;
+                        break;
+                    case "pawn":
+                        chessPieces[x][y] = ChessPiece.PAWN_WHITE;
+                        break;
+
+                }
+            } else {
+                switch (name) {
+                    case "king":
+                        chessPieces[y][x] = ChessPiece.KING_BLACK;
+                        break;
+                    case "queen":
+                        chessPieces[y][x] = ChessPiece.QUEEN_BLACK;
+                        break;
+                    case "rook":
+                        chessPieces[y][x] = ChessPiece.ROOK_BLACK;
+                        break;
+                    case "bishop":
+                        chessPieces[y][x] = ChessPiece.BISHOP_BLACK;
+                        break;
+                    case "knight":
+                        chessPieces[y][x] = ChessPiece.KNIGHT_BLACK;
+                        break;
+                    case "pawn":
+                        chessPieces[y][x] = ChessPiece.PAWN_BLACK;
+                        break;
+
+                }
+                System.out.println("Если Вы хотите выйти - введите EXIT. Если хотите продолжить нажмите ENTER");
+                if ("exit".equalsIgnoreCase(scanner.nextLine())) {
+                    exit = true;
+                }
+            }
+        }
+
     }
 }
--- src/main/java/day17/Task2.java
+++ src/main/java/day17/Task2.java
@@ -4,80 +4,9 @@ public class Task2 {
 
     public static void main(String[] args) {
         ChessPiece[][] chessPieces = new ChessPiece[8][8];
-        chessPieces[0][0] = ChessPiece.ROOK_BLACK;
-        chessPieces[0][1] = ChessPiece.EMPTY;
-        chessPieces[0][2] = ChessPiece.EMPTY;
-        chessPieces[0][3] = ChessPiece.EMPTY;
-        chessPieces[0][4] = ChessPiece.EMPTY;
-        chessPieces[0][5] = ChessPiece.ROOK_BLACK;
-        chessPieces[0][6] = ChessPiece.KING_BLACK;
-        chessPieces[0][7] = ChessPiece.EMPTY;
-
-        chessPieces[1][0] = ChessPiece.EMPTY;
-        chessPieces[1][1] = ChessPiece.ROOK_WHITE;
-        chessPieces[1][2] = ChessPiece.EMPTY;
-        chessPieces[1][3] = ChessPiece.EMPTY;
-        chessPieces[1][4] = ChessPiece.PAWN_BLACK;
-        chessPieces[1][5] = ChessPiece.PAWN_BLACK;
-        chessPieces[1][6] = ChessPiece.EMPTY;
-        chessPieces[1][7] = ChessPiece.PAWN_BLACK;
-
-        chessPieces[2][0] = ChessPiece.PAWN_BLACK;
-        chessPieces[2][1] = ChessPiece.EMPTY;
-        chessPieces[2][2] = ChessPiece.KNIGHT_BLACK;
-        chessPieces[2][3] = ChessPiece.EMPTY;
-        chessPieces[2][4] = ChessPiece.EMPTY;
-        chessPieces[2][5] = ChessPiece.EMPTY;
-        chessPieces[2][6] = ChessPiece.PAWN_BLACK;
-        chessPieces[2][7] = ChessPiece.EMPTY;
-
-        chessPieces[3][0] = ChessPiece.QUEEN_BLACK;
-        chessPieces[3][1] = ChessPiece.EMPTY;
-        chessPieces[3][2] = ChessPiece.EMPTY;
-        chessPieces[3][3] = ChessPiece.BISHOP_WHITE;
-        chessPieces[3][4] = ChessPiece.EMPTY;
-        chessPieces[3][5] = ChessPiece.EMPTY;
-        chessPieces[3][6] = ChessPiece.EMPTY;
-        chessPieces[3][7] = ChessPiece.EMPTY;
-
-        chessPieces[4][0] = ChessPiece.EMPTY;
-        chessPieces[4][1] = ChessPiece.EMPTY;
-        chessPieces[4][2] = ChessPiece.EMPTY;
-        chessPieces[4][3] = ChessPiece.BISHOP_BLACK;
-        chessPieces[4][4] = ChessPiece.PAWN_WHITE;
-        chessPieces[4][5] = ChessPiece.EMPTY;
-        chessPieces[4][6] = ChessPiece.EMPTY;
-        chessPieces[4][7] = ChessPiece.EMPTY;
-
-        chessPieces[5][0] = ChessPiece.EMPTY;
-        chessPieces[5][1] = ChessPiece.EMPTY;
-        chessPieces[5][2] = ChessPiece.EMPTY;
-        chessPieces[5][3] = ChessPiece.EMPTY;
-        chessPieces[5][4] = ChessPiece.BISHOP_WHITE;
-        chessPieces[5][5] = ChessPiece.PAWN_WHITE;
-        chessPieces[5][6] = ChessPiece.EMPTY;
-        chessPieces[5][7] = ChessPiece.EMPTY;
-
-        chessPieces[6][0] = ChessPiece.PAWN_WHITE;
-        chessPieces[6][1] = ChessPiece.EMPTY;
-        chessPieces[6][2] = ChessPiece.EMPTY;
-        chessPieces[6][3] = ChessPiece.QUEEN_WHITE;
-        chessPieces[6][4] = ChessPiece.EMPTY;
-        chessPieces[6][5] = ChessPiece.PAWN_WHITE;
-        chessPieces[6][6] = ChessPiece.EMPTY;
-        chessPieces[6][7] = ChessPiece.PAWN_WHITE;
-
-        chessPieces[7][0] = ChessPiece.EMPTY;
-        chessPieces[7][1] = ChessPiece.EMPTY;
-        chessPieces[7][2] = ChessPiece.EMPTY;
-        chessPieces[7][3] = ChessPiece.EMPTY;
-        chessPieces[7][4] = ChessPiece.EMPTY;
-        chessPieces[7][5] = ChessPiece.ROOK_WHITE;
-        chessPieces[7][6] = ChessPiece.KING_WHITE;
-        chessPieces[7][7] = ChessPiece.EMPTY;
-
-
         ChessBoard chessBoard = new ChessBoard(chessPieces);
+        chessBoard.clearChessBoard();
+        chessBoard.initChessBoard();
         chessBoard.print();
 
 
